ed826f4aec6ae03de0cfebdc61b04b4509573a56
Philipp von Weitershausen <philipp@weitershausen.de>
2011-06-17 16:16:55 +0200

    Switch to using single-item callbacks everywhere (with the exception of guidsCallback) since iterators aren't optimzied in any way. *sadface*
    Adapt existing repository implementations to callback world.
    Implement + test ServerRepository.prototype.guidsSince(), various other fixes.

--- services/sync/modules/repository.js
+++ services/sync/modules/repository.js
@@ -36,6 +36,9 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
+const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
+Cu.import("resource://services-sync/resource.js");
+
 const EXPORTED_SYMBOLS = ["Repository",
                           "ServerRepository",
                           "Crypto5Middleware"];
@@ -49,20 +52,18 @@ Repository.prototype = {
   /**
    * Values to pass to and from callbacks.
    */
-  DONE: {
-    // DONE is an iterable of length 1 and contains just itself.
-    // This allows us to simply pass DONE whenever an iterator is expected.
-    __iterator__: function() {
-      return (function() {
-        yield Repository.prototype.DONE;
-      })();
-    }
-  },
+  DONE: {},
   STOP: {},
 
   /**
    * Retrieve a sequence of GUIDs corresponding to records that have been
-   * modified since timestamp.
+   * modified since timestamp. The callback is invoked exactly once.
+   *
+   * @param timestamp
+   *        Number of seconds since the epoch (can be a decimal number).
+   * @param guidsCallback
+   *        Callback function with the signature (error, guids_array).
+   *        @param error is null for a successful operation.
    */
   guidsSince: function guidsSince(timestamp, guidsCallback) {
     throw "Repository must implement 'guidsSince'";
@@ -70,8 +71,16 @@ Repository.prototype = {
 
   /**
    * Retrieve a sequence of records that have been modified since timestamp.
-   * Invoke the callback with one or more records each time. The last record
-   * will always be the DONE object.
+   * Invoke the callback once for each retrieved record and finally with
+   * the DONE value.
+   *
+   * @param timestamp
+   *        Number of seconds since the epoch (can be a decimal number).
+   * @param fetchCallback
+   *        Callback function with the signature (error, record).
+   *        @param error is null for a successful operation.
+   *        @param record will be the DONE value on the last invocation.
+   *        @return STOP if the fetch operation should be aborted,
    */
   fetchSince: function fetchSince(timestamp, fetchCallback) {
     throw "Repository must implement 'fetchSince'";
@@ -79,15 +88,34 @@ Repository.prototype = {
 
   /**
    * Retrieve a sequence of records by GUID. guids should be an iterable.
-   * Invoke the callback, as with fetchSince.
+   * Invoke the callback once for each retrieved record and finally with
+   * the DONE value.
+   *
+   * @param guids
+   *        Array of GUIDs to retrieve.
+   * @param fetchCallback
+   *        Callback function with the signature (error, record).
+   *        @param error is null for a succcessful operation.
+   *        @param record will be the DONE value on the last invocation.
+   *        @return STOP if the fetch operation should be aborted.
    */
   fetch: function fetch(guids, fetchCallback) {
     throw "Repository must implement 'fetch'";
   },
 
   /**
-   * Store the given sequence of records. Invoke the callback with a sequence
-   * of errors, if there were any, and DONE when complete.
+   * Store the given sequence of records. Invoke the callback for each failed
+   * item and finally with the DONE value.
+   *
+   * @param recs
+   *        Array of records.
+   * @param storeCallback
+   *        Callback with the signature (error)
+   *        @param error describes an error. It may be an arbitrary exception
+   *        object, a repository error where error.guid contains the GUID of
+   *        the record that couldn't be stored, or the DONE value to indicate
+   *        the operation has completed.
+   *        @return STOP if the store operation should be aborted.
    */
   store: function store(recs, storeCallback) {
     throw "Repository must implement 'store'";
@@ -114,34 +142,42 @@ ServerRepository.prototype = {
    */
 
   guidsSince: function guidsSince(timestamp, guidsCallback) {
-
+    let resource = new AsyncResource(this.uri + "?newer=" + timestamp);
+    resource.get(function (error, result) {
+      if (error) {
+        guidsCallback(error);
+        return;
+      }
+      try {
+        result = JSON.parse(result);
+      } catch (ex) {
+        //TODO
+        guidsCallback(ex);
+        return;
+      }
+      guidsCallback(null, result);
+    });
   },
 
   fetchSince: function fetchSince(timestamp, fetchCallback) {
-    let uri = this.uri;
+    let uri = this.uri + "?full=1&newer=" + timestamp;
     if (this.downloadLimit) {
-      uri += "limit=newer&sort=index";
+      uri += "&limit=" + this.downloadLimit + "&sort=index";
     }
     this._fetchRecords(uri, fetchCallback);
   },
 
   fetch: function fetch(guids, fetchCallback) {
-    let uri = this.uri;
-    if (!Array.isArray(guids)) {
-      guids = [guid for (guid in guids)];
-    }
-    uri += "ids=" + guids;
+    let uri = this.uri + "full=1&ids=" + guids;
     this._fetchRecords(uri, fetchCallback);
   },
 
   store: function store(recs, storeCallback) {
-    if (!Array.isArray(guids)) {
-      recs = [record for (record in recs)];
-    }
-    //TODO batching
+    //TODO batching? or should this be done by the Synchronizer?
+    // or a batching middleware?
     let resource = new AsyncResource(uri);
     resource.put(recs, function onPut(error, result) {
-      //TODO process result, may contain errors
+      //TODO process result, may contain errors about individual records
       storeCallback(error);
     });
   },
@@ -155,31 +191,27 @@ ServerRepository.prototype = {
     resource.setHeader("Accept", "application/newlines");
 
     //TODO XXX resource._data and resouce._onProgress are so retarded,
+    // ('this' is the ChannelListener, not the resource!)
     // need a better streaming api for resource
 
-    let readyToRead = true;
-    function incomingRecordGenerator() {
-      let newline;
-      //TODO abort on STOP
-      while ((newline = resource._data.indexOf("\n")) > 0) {
-        let json = resource._data.slice(0, newline);
-        resource._data = resource._data.slice(newline + 1);
-        yield JSON.parse(json);
-      }
-      readyToRead = true;
-    }
-
     resource._onProgress = function onProgress() {
-      if (!readyToRead) {
-        return;
+      let newline;
+      while ((newline = this._data.indexOf("\n")) > 0) {
+        let json = this._data.slice(0, newline);
+        this._data = this._data.slice(newline + 1);
+        let rv, record;
+        try {
+          record = JSON.parse(json);
+        } catch(ex) {
+          //TODO
+          rv = fetchCallback(ex);
+        }
+        rv = fetchCallback(null, record);
+        // TODO process rv, abort on STOP
       }
-      readyToRead = false;
-      let rv = fetchCallback(null, incomingRecordGenerator());
-      // TODO process rv, abort on STOP
     };
     resource.get(function onGet(error, result) {
-      let rv = fetchCallback(error, Repository.prototype.DONE);
-      // TODO process rv, abort on STOP
+      fetchCallback(error, Repository.prototype.DONE);
     });
   }
 
@@ -209,30 +241,41 @@ Crypto5Middleware = {
   },
 
   fetchSince: function fetchSince(timestamp, fetchCallback) {
-    function decryptCallback(errs, recs) {
-      return fetchCallback(errs, (this.decrypt(rec) for (rec in recs)));
-    }
-    this.repository.fetchSince(timestamp, decryptCallback.bind(this));
+    this.repository.fetchSince(timestamp, this.makeDecryptCb(fetchCallback));
   },
 
   fetch: function fetch(guids, fetchCallback) {
-    function decryptCallback(errs, recs) {
-      return fetchCallback(errs, (this.decrypt(rec) for (rec in recs)));
-    }
-    this.repository.fetch(guids, decryptCallback.bind(this));
+    this.repository.fetch(guids, this.makeDecryptCb(fetchCallback));
   },
 
   store: function store(recs, storeCallback) {
-    this.repository.store((this.encrypt(rec) for (rec in recs)), storeCallback);
+    for (let i = 0; i < recs.length; i++) {
+      //XXX we're assuming we own the recs array here, avoids having to create
+      // another array object.
+      //TODO if encryption fails somehow, catch the error and call storeCallback
+      // appropriately.
+      recs[i] = this.encrypt(recs[i]);
+    }
+    this.repository.store(recs, storeCallback);
   },
 
   /**
    * Crypto + storage format stuff
    */
 
+  makeDecryptCb: function makeDecryptCb(fetchCallback) {
+    return (function decryptCallback(error, record) {
+      if (!error && record != DONE) {
+        //TODO if decryption fails somehow, catch the error and call
+        // fetchCallback appropriately.
+        record = this.decrypt(record);
+      }
+      return fetchCallback(error, record);
+    }).bind(this);
+  },
+
   //XXX TODO this doesn't handle errors and key refetches very well
-  // idea: catch exceptions in the recs iterator and don't invoke callback
-  // until we have keys.
+  // idea: catch exceptions above and don't invoke callback until we have keys.
   encrypt: function encrypt(record, keyBundle) {
     keyBundle = keyBundle || CollectionKeys.keyForCollection(this.collection);
     if (!keyBundle)

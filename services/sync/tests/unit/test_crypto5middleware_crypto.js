ad46b4eb3126f87da57123e5063102245075c42a
Philipp von Weitershausen <philipp@weitershausen.de>
2011-06-21 02:53:36 +0200

    Finish and test Crypto5Middleware.
    Still missing tests for the actual crypto bits, but the repository and record <--> WBO translation should be covered here.

---
+++ services/sync/tests/unit/test_crypto5middleware_crypto.js
@@ -0,0 +1,16 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+Cu.import("resource://services-sync/repository.js");
+
+function run_test() {
+  run_next_test();
+}
+
+add_test(function test_encrypt() {
+  run_next_test(); //TODO
+});
+
+add_test(function test_decrypt() {
+  run_next_test(); //TODO
+});

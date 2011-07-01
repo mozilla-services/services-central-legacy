2be6f9995be8f00723b3b10b818cf75ea17f691e
Richard Newman <rnewman@mozilla.com>
2011-07-01 13:20:04 -0700

    Stub out Synchronizer. Lots of open questions.

---
+++ services/sync/tests/unit/test_synchronizer.js
@@ -0,0 +1,13 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+Cu.import("resource://services-sync/repository.js");
+Cu.import("resource://services-sync/synchronizer.js");
+Cu.import("resource://services-sync/log4moz.js");
+
+/**
+ * Test syncing two repositories.
+ */
+function run_test() {
+  run_next_test();
+}

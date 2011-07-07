0cb2f6de0ffab471b80979927142197d40e7aac9
Richard Newman <rnewman@mozilla.com>
2011-07-07 11:34:41 -0700

    Starting to build the skeleton of Synchronizer.

--- services/sync/tests/unit/test_synchronizer.js
+++ services/sync/tests/unit/test_synchronizer.js
@@ -5,9 +5,34 @@ Cu.import("resource://services-sync/repository.js");
 Cu.import("resource://services-sync/synchronizer.js");
 Cu.import("resource://services-sync/log4moz.js");
 
-/**
- * Test syncing two repositories.
- */
 function run_test() {
   run_next_test();
 }
+
+add_test(function test_empty_repositories() {
+  _("Test syncing two empty repositories.");
+  let r1 = new WBORepository();
+  let r2 = new WBORepository();
+  let s1 = new Synchronizer();
+  s1.repositoryA = r1;
+  s1.repositoryB = r2;
+  function synchronizeCallback(error) {
+    do_check_true(!error);
+    do_check_eq(0, Object.keys(r1.wbos).length);
+    do_check_eq(0, Object.keys(r2.wbos).length);
+    // TODO: uncomment when Synchronizer is implemented.
+    //do_check_true(s1.lastSyncLocal > 0);
+    run_next_test();
+  }
+  s1.synchronize(synchronizeCallback);
+});
+
+add_test(function test_empty_to_full() {
+  _("Test syncing an empty repository to a full repository.");
+  run_next_test();
+});
+
+add_test(function test_full_to_empty() {
+  _("Test syncing a full repository to an empty repository.");
+  run_next_test();
+});

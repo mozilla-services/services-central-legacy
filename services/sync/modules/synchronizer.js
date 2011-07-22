a0306e5a232d21765dfc2aace80b25bd33362031
Richard Newman <rnewman@mozilla.com>
2011-07-21 17:21:28 -0700

    Comment and rearrange Synchronizer.

--- services/sync/modules/synchronizer.js
+++ services/sync/modules/synchronizer.js
@@ -48,12 +48,24 @@ const EXPORTED_SYMBOLS = ["Synchronizer"];
  * As with other kinds of session, this is a one-shot object.
  *
  * SynchronizerSession is an implementation detail of the Synchronizer. It is
- * not a public class.
+ * not a public class. Synchronizer interacts with SynchronizerSession through
+ * three callbacks:
  *
- * Grab a session for each of our repositories. Once both sessions are set
- * up, we pair invocations of fetchSince and store callbacks, switching
- * places once the first stream is done. Then we finish each session and
- * invoke our callback.
+ *   - onInitialized, invoked when the session has been established after calling 'init';
+ *   - onSynchronized, invoked when synchronization has completed;
+ *   - onFetchError, invoked when a fetch failed (and accepts a return val);
+ *   - onStoreError, when storing an item failed;
+ *   - onSessionError, when beginning a RepositorySession failed.
+ *
+ * and two methods:
+ *
+ *   - init, which will ultimately cause onInitialized to be invoked;
+ *   - synchronize, which will result in onSynchronized being called.
+ *
+ * SynchronizerSession grabs a session for each of our repositories. Once both
+ * sessions are set up, we pair invocations of fetchSince and store callbacks,
+ * switching places once the first stream is done. Then we finish each session
+ * and invoke a callback.
  *
  * Example usage:
  *
@@ -91,9 +103,10 @@ SynchronizerSession.prototype = {
   bundleB:     null,
 
   /**
-   * Override these two methods!
-   * TODO: comments
+   * Synchronizer interface.
+   * Override these methods!
    */
+
   onInitialized: function onInitialized(error) {
     throw "Override onInitialized.";
   },
@@ -103,71 +116,46 @@ SynchronizerSession.prototype = {
   },
 
   /**
+   * Invoked when a fetch of a record has failed. The arguments are the same as
+   * for the callback on `RepositorySession.fetchSince`.
+   *
    * Override this if you want to terminate fetching, or apply
-   * other recovery/etc. handling.
+   * other recovery/etc. handling, when failure occurs.
    */
   onFetchError: function onFetchError(error, record) {
     // E.g., return Repository.prototype.STOP;
   },
 
   /**
+   * Invoked when storage of a record has failed. This mirrors
+   * `RepositorySession.storeCallback`.
+   *
    * storeCallback doesn't admit any kind of control flow, so only bother
    * overriding this if you want to watch what's happening.
    */
   onStoreError: function onStoreError(error) {
   },
 
-  // TODO
+  /**
+   * Invoked when a RepositorySession could not be established.
+   */
   onSessionError: function onSessionError(error) {
   },
 
-  // TODO: bind and currying session.
-  fetchCallback: function fetchCallback(session, error, record) {
-    if (error) {
-      this._log.warn("Got error " + Utils.exceptionStr(error) +
-                     " fetching. Invoking onFetchError for handling.");
-      // Return the handler value, which allows the caller to do useful things
-      // like abort.
-      return this.onFetchError(error, record);
-    }
-    session.store(record);
-    return null;
+  /**
+   * Initialize the two repository sessions, then invoke onInitialized.
+   * This is a public method.
+   */
+  init: function init() {
+    this.synchronizer
+        .repositoryA.createSession(this.storeCallbackA.bind(this),
+                                   this.sessionCallbackA.bind(this));
   },
 
   /**
-   * The two storeCallbacks are instrumental in switching sync direction and
-   * actually finishing the sync. This is where the magic happens.
+   * Creating a session invokes these two callbacks. We chain them to create
+   * both sessions.
    */
-  storeCallbackB: function storeCallbackB(error) {
-    if (error != Repository.prototype.DONE) {
-      // Hook for handling. No response channel yet.
-      this.onStoreError(error);
-      return;
-    }
-    this._log.trace("Done with records in storeCallbackB.");
-    this._log.trace("Fetching from B into A.");
-    // On to the next!
-    this.sessionB.begin(function (err) {
-      if (err) {
-        // Hook for handling. No response channel yet.
-        this.onSessionError(error);
-        return;
-      }
-      this.sessionB.fetchSince(this.synchronizer.lastSyncB,
-                               this.fetchCallback.bind(this, this.sessionA));
-    }.bind(this));
-  },
-
-  storeCallbackA: function storeCallbackA(error) {
-    this._log.debug("In storeCallbackA().");
-    if (error != Repository.prototype.DONE) {
-      this.onStoreError(error);
-      return;
-    }
-    this._log.trace("Done with records in storeCallbackA.");
-    this.finishSync();
-  },
-
   sessionCallbackA: function sessionCallbackA(error, session) {
     if (error) {
       this.onInitialized(error);
@@ -191,6 +179,79 @@ SynchronizerSession.prototype = {
   },
 
   /**
+   * Assuming that two sessions have been initialized, sync, then clean up and
+   * invoke onSynchronized.
+   */
+  synchronize: function synchronize() {
+    this._log.trace("Fetching from A into B.");
+    let timestamp = this.synchronizer.lastSyncA;
+    this.synchronizeSessions(this.sessionA, this.sessionB, timestamp);
+  },
+
+  /**
+   * Begin the `from` session, fetching records since `timestamp` into `to`.
+   * We use this method in each direction in turn: once in `synchronize`, and
+   * once in a callback that indicates that the first direction is done.
+   */
+  synchronizeSessions: function synchronizeSessions(from, to, timestamp) {
+    from.begin(function (err) {
+      if (err) {
+        // Hook for handling. No response channel yet.
+        this.onSessionError(err);
+        return;
+      }
+      from.fetchSince(timestamp, this.fetchCallback.bind(this, to));
+    }.bind(this));
+  },
+
+  /**
+   * Internal callback for fetched records. `bind` is used to curry the value
+   * of `session`, allowing us to use one callback for both directions.
+   */
+  fetchCallback: function fetchCallback(session, error, record) {
+    if (error) {
+      this._log.warn("Got error " + Utils.exceptionStr(error) +
+                     " fetching. Invoking onFetchError for handling.");
+      // Return the handler value, which allows the caller to do useful things
+      // like abort.
+      return this.onFetchError(error, record);
+    }
+    session.store(record);
+    return null;
+  },
+
+  /**
+   * The two storeCallbacks are instrumental in switching sync direction and
+   * actually finishing the sync. This is where the magic happens: each
+   * callback is invoked when storing fails or completes, and so we can flip
+   * directions (for the first) and invoke the output callback (for the
+   * second).
+   */
+  storeCallbackB: function storeCallbackB(error) {
+    if (error != Repository.prototype.DONE) {
+      // Hook for handling. No response channel yet.
+      this.onStoreError(error);
+      return;
+    }
+    this._log.trace("Done with records in storeCallbackB.");
+    this._log.trace("Fetching from B into A.");
+
+    // On to the next!
+    let timestamp = this.synchronizer.lastSyncB;
+    this.synchronizeSessions(this.sessionB, this.sessionA, timestamp);
+  },
+
+  storeCallbackA: function storeCallbackA(error) {
+    this._log.debug("In storeCallbackA().");
+    if (error != Repository.prototype.DONE) {
+      this.onStoreError(error);
+      return;
+    }
+    this._log.trace("Done with records in storeCallbackA.");
+    this.finishSync();
+  },
+
+  /**
    * Dispose of both sessions and invoke onSynchronized.
    */
   finishSync: function finishSync() {
@@ -205,31 +266,6 @@ SynchronizerSession.prototype = {
       }.bind(this));
     }.bind(this));
   },
-
-  /**
-   * Initialize the two repository sessions, then invoke onInitialized.
-   */
-  init: function init() {
-    this.synchronizer
-        .repositoryA.createSession(this.storeCallbackA.bind(this),
-                                   this.sessionCallbackA.bind(this));
-  },
-
-  /**
-   * Assuming that two sessions have been initialized, sync, then clean up and
-   * invoke onSynchronized.
-   */
-  synchronize: function synchronize() {
-    this.sessionA.begin(function (err) {
-      if (err) {
-        // Hook for handling. No response channel yet.
-        this.onSessionError(err);
-        return;
-      }
-      this.sessionA.fetchSince(this.synchronizer.lastSyncA,
-                               this.fetchCallback.bind(this, this.sessionB));
-    }.bind(this));
-  }
 };
 
 /**

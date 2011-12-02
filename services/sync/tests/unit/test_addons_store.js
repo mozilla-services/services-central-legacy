aa48e49dc28b6f69a38336c4f67f0c81e8a00518
Gregory Szorc <gps@mozilla.com>
2011-12-02 12:38:53 -0800

    store xpcshell test work - they pass!

--- services/sync/tests/unit/test_addons_store.js
+++ services/sync/tests/unit/test_addons_store.js
@@ -5,6 +5,8 @@
 
 Cu.import("resource://services-sync/engines/addons.js");
 
+Svc.Prefs.set("addons.ignoreRepositoryChecking", true);
+
 loadAddonTestFunctions();
 startupManager();
 
@@ -12,6 +14,7 @@ Engines.register(AddonsEngine);
 let engine = Engines.get("addons");
 let tracker = engine._tracker;
 let store = engine._store;
+let reconciler = engine._reconciler;
 
 const uriPrefix = "http://localhost:4444/addons/";
 
@@ -41,30 +44,49 @@ function run_test() {
   run_next_test();
 }
 
-// Note that the following tests, which aim to test application of changes,
-// actually call applyIncomingBatch(). This is by design. We shouldn't be
-// doing anything here we don't test above in the change-generation code. So,
-// we end up with a little redundant testing. But, we save writing extra tests
-// for applyIncomingBatch on top of _applyChanges.
+add_test(function test_get_all_ids() {
+  _("Ensures that getAllIDs() returns an appropriate set.");
 
-add_test(function test_apply_guid_changes() {
-  _("Ensures that add-on GUIDs are updated properly.");
+  engine._refreshReconcilerState();
+
+  let addon1 = installAddon("test_install1");
+  let addon2 = installAddon("test_install2_1");
+
+  let ids = store.getAllIDs();
+  do_check_eq("object", typeof(ids));
+  do_check_eq(2, Object.keys(ids).length);
+  do_check_true(addon1.syncGUID in ids);
+  do_check_true(addon2.syncGUID in ids);
+
+  uninstallAddon(addon1);
+  uninstallAddon(addon2);
+
+  run_next_test();
+});
+
+add_test(function test_change_item_id() {
+  _("Ensures that changeItemID() works properly.");
 
   let addon = installAddon("test_install1");
 
-  let records = [];
-  let newGUID = Utils.makeGUID();
+  let oldID = addon.syncGUID;
+  let newID = Utils.makeGUID();
 
-  records.push(createRecordForThisApp(newGUID, addon.id, true, false));
-  let failed = store.applyIncomingBatch(records);
-  do_check_eq(0, failed.length);
+  store.changeItemID(oldID, newID);
 
   let newAddon = getAddonFromAddonManagerByID(addon.id);
   do_check_neq(null, newAddon);
-  do_check_eq(addon.id, newAddon.id);
-  do_check_eq(newGUID, newAddon.syncGUID);
+  do_check_eq(newID, newAddon.syncGUID);
 
-  newAddon.uninstall();
+  uninstallAddon(newAddon);
+
+  run_next_test();
+});
+
+add_test(function test_create() {
+  _("Ensure creating/installing an add-on from a record works.");
+
+  // TODO
 
   run_next_test();
 });
@@ -92,9 +114,47 @@ add_test(function test_apply_enabled() {
   addon = getAddonFromAddonManagerByID(addon.id);
   do_check_false(addon.userDisabled);
 
-  records = [];
+  uninstallAddon(addon);
 
-  addon.uninstall();
+  run_next_test();
+});
+
+add_test(function test_ignore_different_appid() {
+  _("Ensure that incoming records with a different application ID are ignored.");
+
+  // We test by creating a record that should result in an update.
+  let addon = installAddon("test_install1");
+  do_check_false(addon.userDisabled);
+
+  let record = createRecordForThisApp(addon.syncGUID, addon.id, false, false);
+  record.applicationID = "FAKE_ID";
+
+  let failed = store.applyIncomingBatch([record]);
+  do_check_eq(0, failed.length);
+
+  let newAddon = getAddonFromAddonManagerByID(addon.id);
+  do_check_false(addon.userDisabled);
+
+  uninstallAddon(addon);
+
+  run_next_test();
+});
+
+add_test(function test_ignore_unknown_source() {
+  _("Ensure incoming records with unknown source are ignored.");
+
+  let addon = installAddon("test_install1");
+
+  let record = createRecordForThisApp(addon.syncGUID, addon.id, false, false);
+  record.source = "DUMMY_SOURCE";
+
+  let failed = store.applyIncomingBatch([record]);
+  do_check_eq(0, failed.length);
+
+  let newAddon = getAddonFromAddonManagerByID(addon.id);
+  do_check_false(addon.userDisabled);
+
+  uninstallAddon(addon);
 
   run_next_test();
 });
@@ -115,6 +175,7 @@ add_test(function test_apply_uninstall() {
   run_next_test();
 });
 
+/*
 add_test(function test_wipe() {
   _("Ensures that wiping causes add-ons to be uninstalled.");
 
@@ -130,3 +191,4 @@ add_test(function test_wipe() {
 
   run_next_test();
 });
+*/
